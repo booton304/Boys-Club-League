d1f64e5b5e5a1fa7775f48ad7d46c275d5ceb941
booton304 <41555101+booton304@users.noreply.github.com>
2026-07-29 23:38:17 -0400

    Integrate FullPost component and add styles
    Refactor blog post page to include FullPost component and styling.

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,12 +1,38 @@
 <script>
-  export let data;
+    import { FullPost } from "$lib/components";
+
+    export let data;
+    const {postsData, postID, leagueTeamManagersData} = data;
 </script>
 
-<article>
-  <h1>{data.meta.title}</h1>
-  <p>Written by {data.meta.author} on {data.meta.date}</p>
-  
-  <hr />
-  
-  <svelte:component this={data.Content} />
-</article>
+<style>
+    #main {
+        position: relative;
+        z-index: 1;
+        display: block;
+        margin: 30px auto;
+		width: 95%;
+		max-width: 1000px;
+        overflow-y: hidden;
+    }
+
+    .center {
+        text-align: center;
+        margin-bottom: 2em;
+    }
+
+    .viewAll {
+        text-decoration: none;
+        background-color: #920505;
+        color: #fff;
+        border-radius: 1em;
+        padding: 0.5em 1em;
+    }
+</style>
+
+<div id="main">
+    <FullPost {postsData} {postID} {leagueTeamManagersData} />
+    <div class="center">
+        <a class="viewAll" href="/blog">View More Blog Posts</a>
+    </div>
+</div>
